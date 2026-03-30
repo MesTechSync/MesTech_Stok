b9f14aa1f8c84cfd709e8bef971aea8ad939e747
MesTech Sync <mestech@deploy>
2026-03-30 09:59:13 +0300

    fix(infra): correct namespace in AdapterDecoratorExtensions.cs

--- MesTechStok/src/MesTech.Infrastructure/DependencyInjection/AdapterDecoratorExtensions.cs
+++ MesTechStok/src/MesTech.Infrastructure/DependencyInjection/AdapterDecoratorExtensions.cs
@@ -1,4 +1,4 @@
-using MesTech.Application.Interfaces;
+﻿using MesTech.Application.Interfaces;
 using MesTech.Infrastructure.Integration.Adapters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
